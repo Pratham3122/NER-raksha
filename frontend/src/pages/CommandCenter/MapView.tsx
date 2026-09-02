dfdef46a2be2430613fa762f574327497c6982d6
Pratham3122 <prathamshah3122007@gmail.com>
2026-09-03 03:56:54 +0530

    Update MapView.tsx

--- frontend/src/pages/CommandCenter/MapView.tsx
+++ frontend/src/pages/CommandCenter/MapView.tsx
@@ -56,13 +56,13 @@ export const MapView: React.FC = () => {
           <LayersControl.BaseLayer checked name="Dark Map (English)">
             <TileLayer
               className="dark-tiles"
-              url="https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png"
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | Wikimedia'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
           </LayersControl.BaseLayer>
           <LayersControl.BaseLayer name="Standard Map (English)">
             <TileLayer
-              url="https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png"
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | Wikimedia'
             />
           </LayersControl.BaseLayer>
